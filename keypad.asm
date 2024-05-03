86cae8d0b9e0aa7b1450c53eda294ab0aba73779
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2024-05-03 11:02:49 +0200

    macros et definitions

--- keypad.asm
+++ keypad.asm
@@ -1,9 +1,7 @@
+; file  keypad.asm target ATmega128L
+; purpose main file
+
 keypad:
-    ldi     r16, 0x0f
-    out     DDRD, r16
-    ldi     r20, 0b11101111
-    out     PORTD, r20
-    nop
-    in      r16, PIND
-    andi    r16, 0b11110000
-    ret
+        in      r31, PINF
+
+        ret

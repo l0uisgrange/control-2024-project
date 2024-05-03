40b735d910639fa3826d2b3452007ae66a3769d5
GhostTraderX <126263543+GhostTraderX@users.noreply.github.com>
2024-05-03 10:51:33 +0200

    LCD initialization

--- app.asm
+++ app.asm
@@ -1,6 +1,14 @@
 ; file  app.asm target ATmega128L
 ; purpose main file
 
+.include "lcd.asm"
+
+
 reset:
-        ldi     r31, 0x00   ; configure portF as output
+        ldi     r31, 0x00       ; configure portF as input
         out     DDRF, r31
+        ;       ---------       ; LCD reset
+        in      r16, MCUCR      ; enable ext. SRAM access
+        sbr     sbr, (1<<SRE)+(1<<SRW10)
+
+

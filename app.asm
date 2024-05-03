479d315014793cdf71da322b36be08ddf0ef30f4
GhostTraderX <126263543+GhostTraderX@users.noreply.github.com>
2024-05-03 11:31:31 +0200

    LCD test display

--- app.asm
+++ app.asm
@@ -1,6 +1,7 @@
 ; file  app.asm target ATmega128L
 ; purpose main file
 
+; === include ===
 .include "lcd.asm"
 
 ; interrupt vector table
@@ -10,14 +11,34 @@
 
 ; reset before start
 reset:
-        ldi     r31, 0x00       ; configure portF as input
+        ldi     r31, 0x00           ; configure portF as input
         out     DDRF, r31
-        ;       ---------       ; LCD reset
-        in      r16, MCUCR      ; enable ext. SRAM access
-        sbr     sbr, (1<<SRE)+(1<<SRW10)
+        ;       --------------      ; START LCD resest
+        LDSP    RAMEND              ; set up stack pointer
+        OUTI    DDRB, 0xff          ; configure portB to output
+        rcall   LCD_init            ; initialize LCD
+        rcall   LCD_blink_on        ; turn blinking on
+        jmp     intro
+        ;       --------------      ; END LCD reset
         rjmp main
 
+
+intro:
+        ldi     a0, 'A'             ; write character 'A'
+        rcall   lcd_putc
+        ldi     a0, 'V'             ; write character 'V'
+        rcall   lcd_putc
+        ldi     a0, 'R'             ; write character 'R'
+        rcall   lcd_putc
+
+
 main:
-        ; code
-
-
+        WAIT_MS 100
+        CP0     PIND, 0, LCD_home
+        CP0     PIND, 1, LCD_clear
+        CP0     PIND, 2, LCD_display_right
+        CP0     PIND, 3, LCD_display_left
+        CP0     PIND, 4, LCD_cursor_right
+        CP0     PIND, 5, LCD_cursor_left
+        JP0     PIND, 6, intro
+        rjmp    main

e85ef49f031e63fa409b447314dba95131c85385
GhostTraderX <126263543+GhostTraderX@users.noreply.github.com>
2024-05-06 16:48:09 +0200

    version prof

--- app.asm
+++ app.asm
@@ -18,7 +18,7 @@ reset:
         STSI    EICRA, ((1<<ISC01) | (1<<ISC00))
         sei                             ; set global interrupt
         ;       --------------          ; configure PORTA as I/O
-        OUTI    DDRD, 1
+        OUTI    DDRD, 0b11111111
         OUTI    PORTD, KPD_COL          ; set bits 0-3 to 
         ;       --------------          ; START LCD reset
         rcall   LCD_init                ; initialize LCD

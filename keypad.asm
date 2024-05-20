21b9941e32d9cb5b1c842ab7d8190ccacba23699
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2024-05-20 15:14:39 +0200

    Update keypad.asm

--- keypad.asm
+++ keypad.asm
@@ -9,87 +9,86 @@
 .def	sem = r15		; semaphore: must enter LCD display routine, unary: 0 or other
 
 .org 0
-	jmp reset
-	jmp isr_ext_int0	; external interrupt INT0
-	jmp isr_ext_int1	; external interrupt INT1
-	jmp isr_ext_int2	; external interrupt INT2
-	jmp isr_ext_int3	; external interrupt INT3
+	jmp 	reset
+	jmp 	isr_ext_int0		; external interrupt INT0
+	jmp 	isr_ext_int1		; external interrupt INT1
+	jmp 	isr_ext_int2		; external interrupt INT2
+	jmp 	isr_ext_int3		; external interrupt INT3
 
 isr_ext_int0:
-	;in _sreg, SREG			;sauvegarde du contexte
-	INVP PORTB,0			;debug
-	_LDI	row, 0x00		; detect row 0
+	INVP 	PORTB, 0		;debug
+	_LDI	row, 0x00		;detect row 0
 	_LDI	mask, 0b00000001
-	out SREG, _sreg			;restauration du contexte
+	out 	SREG, _sreg		;restauration du contexte
 	rjmp	column_detect
 
 isr_ext_int1:
-	INVP PORTB,1			;debug
-	_LDI	row, 0x01		; detect row 1
+	INVP	PORTB, 1		;debug
+	_LDI	row, 0x01		;detect row 1
 	_LDI	mask, 0b00000010
 	rjmp	column_detect
 
 isr_ext_int2:
-	INVP PORTB,2			;debug
-	_LDI	row, 0x02		; detect row 2
+	INVP	PORTB, 2		;debug
+	_LDI	row, 0x02		;detect row 2
 	_LDI	mask, 0b00000100
 	rjmp	column_detect
 
 isr_ext_int3:
-	INVP PORTB,3			;debug
-	_LDI	row, 0x03		; detect row 1
+	INVP	PORTB, 3		;debug
+	_LDI	row, 0x03		;detect row 1
 	_LDI	mask, 0b00001000
 	rjmp	column_detect
 
 column_detect:
-	OUTI	PORTD,0xff	; bit4-7 driven high (lines)
+	OUTI	PORTD, 0xff		;bit4-7 driven high (lines)
 
 col3:					
 	WAIT_MS	KPD_DELAY
-	OUTI	PORTD,0b01111111	; check column 3
+	OUTI	PORTD, 0b01111111	; check column 3
 	WAIT_MS	KPD_DELAY
-	in	w,PIND		; in PIND
-	and	w,mask		; we are masking the selected row
+	in	w, PIND		; in PIND
+	and	w, mask		; we are masking the selected row
 	tst	w			; testing if column is pressed (test for 0 or minus)
 	brne	col2
-	_LDI	col,0x03
-	INVP	PORTB,7		; LED inverts if key pressed!
+	_LDI	col, 0x03
+	INVP	PORTB, 7		; LED inverts if key pressed!
 	rjmp	isr_return
 
 col2:
 	WAIT_MS	KPD_DELAY
-	OUTI	PORTD,0b10111111	; check column 2
+	OUTI	PORTD, 0b10111111	; check column 2
 	WAIT_MS	KPD_DELAY
-	in	w,PIND		; in PIND
-	and	w,mask		
+	in	w, PIND		; in PIND
+	and	w, mask		
 	tst	w			; testing if column is pressed (test for 0 or minus)
 	brne	col1
-	_LDI	col,0x02
-	INVP	PORTB,6		; LED inverts if key pressed!
+	_LDI	col, 0x02
+	INVP	PORTB, 6		; LED inverts if key pressed!
 	rjmp	isr_return
 
 col1:
 	WAIT_MS	KPD_DELAY
-	OUTI	PORTD,0b11011111	; check column 1
+	OUTI	PORTD, 0b11011111	; check column 1
 	WAIT_MS	KPD_DELAY
-	in	w,PIND		; in PIND
-	and	w,mask		
+	in	w, PIND		; in PIND
+	and	w, mask		
 	tst	w			; testing if column is pressed (test for 0 or minus)
 	brne	col0
-	_LDI	col,0x01
-	INVP	PORTB,5		; LED inverts if key pressed!
+	_LDI	col, 0x01
+	INVP	PORTB, 5		; LED inverts if key pressed!
 	rjmp	isr_return
 
 col0:
 	WAIT_MS	KPD_DELAY
-	OUTI	PORTD,0b11101111	; check column 0
+	OUTI	PORTD, 0b11101111	; check column 0
 	WAIT_MS	KPD_DELAY
-	in	w,PIND		; in PIND
-	and	w,mask		
+	in	w, PIND		; in PIND
+	and	w, mask		
 	tst	w			; testing if column is pressed (test for 0 or minus)
 	brne	isr_return
-	_LDI	col,0x00
-	INVP	PORTB,4		; LED inverts if key pressed!
+	_LDI	col, 0x00
+	INVP	PORTB, 4		; LED inverts if key pressed!
 	rjmp	isr_return
 
 isr_return:
@@ -104,11 +103,11 @@ isr_return:
 reset:	
 	LDSP	RAMEND		; Load Stack Pointer (SP)
 	rcall	LCD_init		; initialize UART
-	OUTI	DDRD,0xf0		; bit0-3 pull-up and bits4-7 driven low
-	OUTI	PORTD,0x0f		;>(needs the two lines)
-	OUTI	DDRB,0xff		; turn on LEDs
-	OUTI	EIMSK,0x0f		; enable INT0-INT3
-	OUTI	EICRB,0b0		;>at low level
+	OUTI	DDRD, 0xf0		; bit0-3 pull-up and bits4-7 driven low
+	OUTI	PORTD, 0x0f		;>(needs the two lines)
+	OUTI	DDRB, 0xff		; turn on LEDs
+	OUTI	EIMSK, 0x0f		; enable INT0-INT3
+	OUTI	EICRB, 0b0		;>at low level
 	PRINTF  LCD
 	.db	CR, "Welcome to", CR, LF, "Mastermind"
 	.db     0

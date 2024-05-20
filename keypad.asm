0c939eccf89d757d3280bec8ab203a5ce04e3e7a
GhostTraderX <126263543+GhostTraderX@users.noreply.github.com>
2024-05-20 08:59:18 +0200

    fresh start

--- keypad.asm
+++ keypad.asm
@@ -1,116 +1,137 @@
-; file keypad.asm
+; file	kpd4x4_S.asm   target ATmega128L-4MHz-STK300		
+; purpose keypad 4x4 acquisition and print
+;  uses four external interrupts and ports internal pull-up
 
-.include "macros.asm"
-.include "definitions.asm"
+; solutions based on the methodology presented in EE208-MICRO210_ESP-2024-v1.0.fm
+;>alternate solutions also possible
+; standalone solution/file; not meeant as a modular solution and thus must be
+;>adapted when used in a complex project
+; solution based on interrupts detected on each row; not optimal but functional if
+;>and external four-input gate is not available
 
-; ——————————————— definitions ————————————————
-.equ	KPD_DELAY = 30          ; keypad bouncing delay
-.def	wr0 = r2	        ; detected row in hex
+.include "macros.asm"		; include macro definitions
+.include "definitions.asm"	; include register/constant definitions
+
+	; === definitions ===
+.equ	KPDD = DDRD
+.equ	KPDO = PORTD
+.equ	KPDI = PIND
+
+.equ	KPD_DELAY = 30	; msec, debouncing keys of keypad
+
+.def	wr0 = r2		; detected row in hex
 .def	wr1 = r1		; detected column in hex
 .def	mask = r14		; row mask indicating which row has been detected in bin
-.def	wr2 = r15	        ; semaphore: must enter LCD display routine, unary: 0 or other
+.def	wr2 = r15		; semaphore: must enter LCD display routine, unary: 0 or other
 
-; —————————— interrupt vector table ——————————
+	; === interrupt vector table ===
 .org 0
-	jmp     reset
-	jmp	ext_int0        ; external interrupt INT0
-	jmp	ext_int1        ; external interrupt INT1
+	jmp reset
+	jmp	isr_ext_int0	; external interrupt INT0
+	jmp	isr_ext_int1	; external interrupt INT1
 
-; ————————— interrupt service routines ————————
-ext_int0:
-	_LDI	wr0, 0x00
-	_LDI	mask, 0b00000100
-	rjmp	row_detect
 
-ext_int1:
-	_LDI	wr0, 0x00
-	_LDI	mask, 0b00000100
-	rjmp	row_detect
+; TO BE COMPLETED AT THIS LOCATION
 
-row_detect:
+	; === interrupt service routines ===
+isr_ext_int0:
+	;INVP	PORTB,0			;;debug
+	_LDI	wr1, 0x00		; detect row 1
+	_LDI	mask, 0b00000001
+	rjmp	column_detect
+	; no reti (grouped in isr_return)
 
-row1:
+isr_ext_int1:
+
+; TO BE COMPLETED AT THIS LOCATION
+
+column_detect:
+
+	OUTI	PORTD,0xff	; bit4-7 driven high
+col7:
 	WAIT_MS	KPD_DELAY
-	OUTI	PORTD, 0b00001111
+	OUTI	PORTD,0x7f	; check column 7
 	WAIT_MS	KPD_DELAY
-	in	w, PIND
-	in      wr1, PIND
-	and	w, mask
-	tst	w
-	brne	row2
-	_LDI	wr0, 0x07
-	rjmp	int_return
-
-row2:
+	in		w,PIND
+	and		w,mask
+	tst		w
+	brne	col6
+	_LDI	wr0,0x00
+	;INVP	PORTB,7		;;debug
+	rjmp	isr_return
 	
+col6:
 
-int_return:
-	rcall   LCD_blink_off
-        INVP	PORTB, 0         ; visual feedback of key pressed acknowledge
-	ldi     _w, 10           ; sound feedback of key pressed acknowledge
-	_LDI	wr2, 0xff
+; TO BE COMPLETED AT THIS LOCATION
+	
+	err_row0:			; debug purpose and filter residual glitches		
+	;INVP	PORTB,0
+	rjmp	isr_return
+	; no reti (grouped in isr_return)
+
+isr_return:
+	INVP	PORTB,0		; visual feedback of key pressed acknowledge
+	ldi		_w,10		; sound feedback of key pressed acknowledge
+beep01:	
+	
+	; TO BE COMPLETED AT THIS LOCATION
+	
+	_LDI	wr2,0xff
 	reti
 	
 .include "lcd.asm"			; include UART routines
 .include "printf.asm"		; include formatted printing routines
 
-; —————— initialization and configuration ——————
+; === initialization and configuration ===
 
 .org 0x400
 
-reset:	LDSP	RAMEND                  ; Load Stack Pointer (SP)
+reset:	LDSP	RAMEND		; Load Stack Pointer (SP)
 	rcall	LCD_init		; initialize UART
-	OUTI	DDRD, 0xf0		; bit0-3 pull-up and bits4-7 driven low
-	OUTI	PORTD, 0x0f		; output
-	OUTI	DDRB, 0xff		; turn on LEDs
-	OUTI	EIMSK, 0x0f		; enable INT0-INT3
-	OUTI	EICRB, 0b0		;>at low level
-	sbi	DDRE, SPEAKER           ; enable sound
-	PRINTF  LCD
-	.db	CR, "Welcome to", CR, LF, "Mastermind"
-	.db     0
-	; WAIT_MS 3000
-	rcall   LCD_clear
-	rcall   LCD_home
-	PRINTF  LCD
-	.db	CR, "Number to guess:", LF
-	.db     0
-	rcall   LCD_blink_on
-	clr	wr0
-	clr	wr1
-	clr	wr2
-	clr	a1
-	clr	a2
-	clr	a3
-	clr	b1
-	clr	b2
-	clr	b3
+
+	OUTI	DDRD,0xf0		; bit0-3 pull-up and bits4-7 driven low
+	OUTI	PORTD,0x0f		;>(needs the two lines)
+	OUTI	DDRB,0xff		; turn on LEDs
+	OUTI	EIMSK,0x0f		; enable INT0-INT3
+	OUTI	EICRB,0b0		;>at low level
+	sbi		DDRE,SPEAKER	; enable sound
+
+	PRINTF LCD
+.db	CR,CR,"hello world"
+
+	clr		wr0
+	clr		wr1
+	clr		wr2
+
+	clr		a1				
+	clr		a2
+	clr		a3
+	clr		b1
+	clr		b2
+	clr		b3
+
 	sei
+	;jmp	main				; not useful in this case, kept for modularity
 
-; —————————————— main program ——————————————
+	; === main program ===
 main:
-	tst     wr2				; check flag/semaphore
-	breq    main
-	clr	wr2
-	clr	a0
-	clr     a1
-	clr     a2
-	clr     a3
-	add     a0, wr0
-	clr     b0
-	clr     b1
-	clr     b2
-	clr     b3
-	add     b0, wr1
 
-	PRINTF  LCD
-	.db CR,LF,">", FBIN, b
-	.db 0
+	tst		wr2				; check flag/semaphore
+	breq	main
+	clr		wr2	
+
+	clr		a0
+	add		a0, wr1
+	add		a0, wr0
+
+	; TO BE COMPLETED AT THIS LOCATION		; decoding ascii
+	
+	
+PRINTF LCD
+.db	CR,LF,"KPD=",FHEX,a," ascii=",FHEX,b
+.db	0
 	rjmp	main
 	
 ; code conversion table, character set #1 key to ASCII	
-KPDConvTable:
-.db '1', '2', '3', 'A'
-.db '4', '5', '6', 'B'
-.db '7', '8', '9', 'C'
-.db '*', '0', '#', 'D'
+KeySet01:
+.db ; TO BE COMPLETED AT THIS LOCATION

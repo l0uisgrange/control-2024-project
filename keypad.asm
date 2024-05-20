673fe93a826a23eb95421e1d0c1a1573f5cfdb46
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2024-05-20 14:59:47 +0200

    Update keypad.asm

--- keypad.asm
+++ keypad.asm
@@ -1,13 +1,8 @@
 .include "macros.asm"		; include macro definitions
 .include "definitions.asm"	; include register/constant definitions
 
-	; === definitions ===
-.equ	KPDD = DDRD
-.equ	KPDO = PORTD
-.equ	KPDI = PIND
-
+; ——— definitions ———
 .equ	KPD_DELAY = 30	; msec, debouncing keys of keypad
-
 .def	col = r2		; detected column in hex
 .def	row = r1		; detected row in hex
 .def	mask = r14		; row mask indicating which row has been detected in bin
@@ -15,41 +10,35 @@
 
 .org 0
 	jmp reset
-	jmp	isr_ext_int0	; external interrupt INT0
-	jmp	isr_ext_int1	; external interrupt INT1
+	jmp isr_ext_int0	; external interrupt INT0
+	jmp isr_ext_int1	; external interrupt INT1
 	jmp isr_ext_int2	; external interrupt INT2
 	jmp isr_ext_int3	; external interrupt INT3
 
 isr_ext_int0:
 	;in _sreg, SREG			;sauvegarde du contexte
-
 	INVP PORTB,0			;debug
 	_LDI	row, 0x00		; detect row 0
 	_LDI	mask, 0b00000001
-
 	out SREG, _sreg			;restauration du contexte
-
 	rjmp	column_detect
 
 isr_ext_int1:
 	INVP PORTB,1			;debug
 	_LDI	row, 0x01		; detect row 1
 	_LDI	mask, 0b00000010
-
 	rjmp	column_detect
 
 isr_ext_int2:
 	INVP PORTB,2			;debug
 	_LDI	row, 0x02		; detect row 2
 	_LDI	mask, 0b00000100
-
 	rjmp	column_detect
 
 isr_ext_int3:
 	INVP PORTB,3			;debug
 	_LDI	row, 0x03		; detect row 1
 	_LDI	mask, 0b00001000
-
 	rjmp	column_detect
 
 column_detect:
@@ -59,9 +48,9 @@ col3:
 	WAIT_MS	KPD_DELAY
 	OUTI	PORTD,0b01111111	; check column 3
 	WAIT_MS	KPD_DELAY
-	in		w,PIND		; in PIND
-	and		w,mask		; we are masking the selected row
-	tst		w			; testing if column is pressed (test for 0 or minus)
+	in	w,PIND		; in PIND
+	and	w,mask		; we are masking the selected row
+	tst	w			; testing if column is pressed (test for 0 or minus)
 	brne	col2
 	_LDI	col,0x03
 	INVP	PORTB,7		; LED inverts if key pressed!
@@ -71,9 +60,9 @@ col2:
 	WAIT_MS	KPD_DELAY
 	OUTI	PORTD,0b10111111	; check column 2
 	WAIT_MS	KPD_DELAY
-	in		w,PIND		; in PIND
-	and		w,mask		
-	tst		w			; testing if column is pressed (test for 0 or minus)
+	in	w,PIND		; in PIND
+	and	w,mask		
+	tst	w			; testing if column is pressed (test for 0 or minus)
 	brne	col1
 	_LDI	col,0x02
 	INVP	PORTB,6		; LED inverts if key pressed!
@@ -95,9 +84,9 @@ col0:
 	WAIT_MS	KPD_DELAY
 	OUTI	PORTD,0b11101111	; check column 0
 	WAIT_MS	KPD_DELAY
-	in		w,PIND		; in PIND
-	and		w,mask		
-	tst		w			; testing if column is pressed (test for 0 or minus)
+	in	w,PIND		; in PIND
+	and	w,mask		
+	tst	w			; testing if column is pressed (test for 0 or minus)
 	brne	isr_return
 	_LDI	col,0x00
 	INVP	PORTB,4		; LED inverts if key pressed!

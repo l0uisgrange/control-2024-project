86f9648b16d5462edfd48381dc5395c53b1758c9
GhostTraderX <126263543+GhostTraderX@users.noreply.github.com>
2024-05-21 11:58:33 +0200

    added CLR2, 4

--- keypad.asm
+++ keypad.asm
@@ -144,8 +144,6 @@ lookup0:
 	add	zl, w
 	lpm
 	mov	@0, r0
-	_LDI	row, 0
-	_LDI	col, 0	
 .endmacro
 
 main:
@@ -164,6 +162,7 @@ main:
 	WAIT_MS	2000
 	rcall	LCD_clear
 	rcall	LCD_home
+	CLR2	row, col
 guess:
 	; --- guess ---
 	PRINTF	LCD
@@ -179,8 +178,5 @@ guess:
 	.db 0
 done:
 	WAIT_MS	2000
-	clr	a0
-	clr	b0
-	_LDI	col, 0
-	_LDI	row, 0
+	CLR4	a0, b0, row, col
 	rjmp	main

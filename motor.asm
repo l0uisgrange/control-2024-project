45e71268e59a89e46eabf6b3d2ff06d21fc15a7b
GhostTraderX <126263543+GhostTraderX@users.noreply.github.com>
2024-05-22 13:00:22 +0200

    completed motor.asm

--- motor.asm
+++ motor.asm
@@ -5,7 +5,7 @@
 .include "definitions.asm"
 
 .equ	t1 	= 1000			; waiting period in micro-seconds
-.equ	port_mot= PORTA		; port to which motor is connected
+.equ	port_mot= PORTC		; port to which motor is connected
 
 .macro	MOTOR
 	ldi	w,@0
@@ -13,15 +13,16 @@
 	rcall	wait			; wait period
 .endmacro		
 	
-reset:	LDSP	RAMEND		; load stack pointer SP
-	OUTI	DDRA,0x0f		; make motor port output
+reset:	
+	LDSP	RAMEND		; load stack pointer SP
+	OUTI	DDRC,0x0f		; make motor port output
 loop:	
-	MOTOR	0b			; output motor patterns COMPLETE HERE
-	MOTOR	0b	
-	MOTOR	0b
-	MOTOR	0b
-	MOTOR	0b
-	MOTOR	0b
+	MOTOR	0b0101			; output motor patterns COMPLETE HERE
+	MOTOR	0b0001	
+	MOTOR	0b1011
+	MOTOR	0b1010
+	MOTOR	0b1110
+	MOTOR	0b0100
 	rjmp	loop
 	
 wait:	WAIT_US	t1			; wait routine

ded7fc3b0ef40febb701164487bf95e143a74089
GhostTraderX <126263543+GhostTraderX@users.noreply.github.com>
2024-05-22 13:11:06 +0200

    added wait

--- motor.asm
+++ motor.asm
@@ -1,7 +1,8 @@
 ; file	motor.asm   target ATmega128L-4MHz-STK300
 ; purpose stepper motor control
 ; module: M1, output port: PORTC
-.macro	MOTOR
-	ldi	w,@0
-	out	PORTC, w			; output motor pin pattern
+.macro		MOTOR
+	ldi		w,@0
+	out		PORTC, w
+	WAIT_US	1000			; output motor pin pattern
 .endmacro		

71a501b7a9d30cf4e0f9299e070968203093f29c
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2024-05-22 15:27:34 +0200

    Update eeprom.asm

--- eeprom.asm
+++ eeprom.asm
@@ -3,7 +3,7 @@ eeprom_store:
 	rjmp	PC-1		; jump back to previous address
 	out	EEARL, xl	; load EEPROM address low	
 	out	EEARH, xh	; load EEPROM address high
-	out	EEDR, d3	; set EEPROM data register
+	out	EEDR, d0	; set EEPROM data register
 	brie	eeprom_cli	; if I=1 then temporarily disable interrupts
 	sbi	EECR, EEMWE	; set EEPROM Master Write Enable
 	sbi	EECR, EEWE	; set EEPROM Write Enable
@@ -22,5 +22,5 @@ eeprom_load:
 	out	EEARL, xl	
 	out	EEARH, xh
 	sbi	EECR, EERE	; set EEPROM Read Enable
-	in	d3, EEDR	; read data register of EEPROM
+	in	d0, EEDR	; read data register of EEPROM
 	ret

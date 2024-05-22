8417af7ec9231f8ec0dd63a75b62f2171b35e52c
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2024-05-22 15:03:20 +0200

    Switch to EEPROM

--- motor.asm
+++
@@ -1,8 +0,0 @@
-; file	motor.asm   target ATmega128L-4MHz-STK300
-; purpose stepper motor control
-; module: M1, output port: PORTC
-.macro		MOTOR
-	ldi		w,@0
-	out		PORTC, w
-	WAIT_US	1000			; output motor pin pattern
-.endmacro		

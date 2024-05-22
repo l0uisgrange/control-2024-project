a6939b6cb4a816b3a6f5dc0ca2edf920ed987816
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2024-05-22 14:29:33 +0200

    Update motor.asm

--- motor.asm
+++ motor.asm
@@ -1,29 +1,8 @@
 ; file	motor.asm   target ATmega128L-4MHz-STK300
 ; purpose stepper motor control
 ; module: M1, output port: PORTC
-.include "macros.asm"
-.include "definitions.asm"
-
-.equ	t1 	= 1000			; waiting period in micro-seconds
-.equ	port_mot= PORTA		; port to which motor is connected
-
-.macro	MOTOR
-	ldi	w,@0
-	out	port_mot,w			; output motor pin pattern
-	rcall	wait			; wait period
+.macro		MOTOR
+	ldi		w,@0
+	out		PORTC, w
+	WAIT_US	1000			; output motor pin pattern
 .endmacro		
-	
-reset:	
-	LDSP	RAMEND			; load stack pointer SP
-	OUTI	DDRA,0x0f		; make motor port output
-loop:	
-	MOTOR	0b0101			
-	MOTOR	0b0001	
-	MOTOR	0b1011
-	MOTOR	0b1010
-	MOTOR	0b1110
-	MOTOR	0b0100
-	rjmp	loop
-	
-wait:	WAIT_US	t1			; wait routine
-	ret
